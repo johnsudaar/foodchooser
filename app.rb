935a3260d24f0a3c7f48eadac258f97a24ec859e
Jonathan Hurter <jonathan.hurter@gmail.com>
2016-07-18 18:49:41 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+require 'slack-notifier'
+require 'sinatra'
+
+LAST_SENT = Time.now()
+PLACES    = ENV["PLACES"].split(",")
+NOTIFIER  = Slack::Notifier.new ENV["SLACK_HOOK"]
+
+post '/slack' do
+  message = params[:text]
+
+  if message.include? "manger" || (message.include? "non" && Time.now - LAST_SENT < 60 * 5)
+    NOTIFIER.ping "Je vous propose d'aller manger chez #{PLACES.sample}"
+  end
+end
+
