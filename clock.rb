71d1a9e94c0aaac7a1f0f0d2ef8ddfd817c08b74
Jonathan Hurter <jonathan.hurter@gmail.com>
2016-07-18 19:39:50 +0200

    URI parsing

--- clock.rb
+++ clock.rb
@@ -4,7 +4,8 @@ require 'net/http'
 module Clockwork
   handler do |job|
     STDERR.puts "Running #{job}"
-    Net::HTTP.post_form(ENV["MIDI_URL"], "text"=>"manger")
+    uri = URI(ENV["MIDI_URL"])
+    Net::HTTP.post_form(uri, "text"=>"manger")
   end
 
   every(1.day, 'Send notification', :at => ENV["REMIND_TIME"])
