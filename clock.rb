2f268225491796743321e809c1b2619d2d5fd37f
Jonathan HURTER <jonathan.hurter@gmail.com>
2016-08-22 12:28:06 +0200

    Fix: Use trigger word

--- clock.rb
+++ clock.rb
@@ -5,7 +5,7 @@ module Clockwork
   handler do |job|
     STDERR.puts "Running #{job}"
     uri = URI(ENV["MIDI_URL"])
-    Net::HTTP.post_form(uri, "text"=>"manger")
+    Net::HTTP.post_form(uri, "text"=>ENV["TRIGGER_WORD"])
   end
 
   every(1.day, 'Send notification', :at => ENV["REMIND_TIME"])
